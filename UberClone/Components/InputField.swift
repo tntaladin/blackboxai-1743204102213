4596123903c5d5f9830761049897de73bcac8fb0
tntaladin <93888520+tntaladin@users.noreply.github.com>
2025-03-29 02:22:04 +0300

    Add UberClone/Components/InputField.swift 1743204123475

---
+++ UberClone/Components/InputField.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct InputField: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(title)
+                .foregroundColor(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            } else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }
+            
+            Divider()
+        }
+    }
+}
+
+struct InputField_Previews: PreviewProvider {
+    static var previews: some View {
+        InputField(text: .constant(""), title: "Email", placeholder: "name@example.com")
+            .padding()
+    }
+}
